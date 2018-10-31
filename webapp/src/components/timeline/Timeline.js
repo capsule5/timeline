11b0e5751b30f6f43ede32a6a21d4d40d939c459
Sebap <sebastien@capsule5.com>
2018-10-31 19:38:58 +0100

    feat(webapp): setup timeline

---
+++ webapp/src/components/timeline/Timeline.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "./Timeline.scss"
+
+class Timeline extends Component {
+  render() {
+    return (
+      <div className="timeline">
+        timeline
+      </div>
+    )
+  }
+}
+
+export default Timeline
