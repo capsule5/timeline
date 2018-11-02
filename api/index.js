2ddc56749938ea2a004de10e013248952f72e62e
Sebap <sebastien@capsule5.com>
2018-11-02 21:18:08 +0100

    feat(api): use cors to Access-Control-Allow-Origin

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const routerApi = require('./routes/api')
-
+const cors = require('cors')
 
 const app = express()
 app.use(logger('dev'))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api', routerApi)
