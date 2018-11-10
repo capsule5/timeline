f47fb6600f7ed0c245532277b5ec77f729a732f1
Sebap <sebastien@capsule5.com>
2018-11-10 01:56:18 +0100

    feat(webapp): add event label as related timeline

--- webapp/src/components/timeline/Event.js
+++ webapp/src/components/timeline/Event.js
@@ -1,10 +1,11 @@
 import React from "react"
 
 const Event = ({
-  id, title, date_year, deleteEvent,
+  id, title, date_year, date_month, date_day, date_reliability, timeline, deleteEvent,
 }) => {
   return (
     <span className="event">
+      <span className="event__label">{timeline.title}</span>
       <span className="event__date">{date_year}</span> - {title}
       <button
         onClick={ () => {
