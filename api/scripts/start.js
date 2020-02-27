6f3cca02fc485e5a6c90e74b28df3575ae634f0d
Sebap <sebastien@capsule5.com>
2020-02-27 17:33:55 +0100

    chore(api) resove bin path

--- api/scripts/start.js
+++ api/scripts/start.js
@@ -1,4 +1,9 @@
 const { execSync } = require('child_process')
+const path = require('path')
 
-execSync('nodemon ./index.js --exec \"node -r babel-register\"',
-{ stdio: 'inherit' })
+console.log('Current directory:', __dirname)
+const binPath = path.resolve('./node_modules/.bin')
+console.log(binPath)
+
+
+execSync(binPath+'/nodemon ./index.js --exec \"node -r babel-register\"', { stdio: 'inherit' })
